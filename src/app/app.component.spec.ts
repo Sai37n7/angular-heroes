fa7da674f2c9e9e4ed834df81a4eedcfab8ee578
Isai Hernandez Villalpando <isai.12hernandez@gmail.com>
2021-10-14 15:51:20 -0500

    Primer commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heroesAppIsai'`, () => {
+  it(`should have as title 'heroesApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('heroesAppIsai');
+    expect(app.title).toEqual('heroesApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('heroesAppIsai app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('heroesApp app is running!');
   });
 });
